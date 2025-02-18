fa365c7b3579619fc5859595324e3d44e9da41f0
Nikhil <nikhilmali3141@gmail.com>
2025-02-18 10:58:47 +0530

    import spelling fails in index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,65 +1,14 @@
-// import express, { urlencoded } from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import connectDB from './Utils/db.js';
-// import userRoute from './Routes/user.route.js';
-// import postRoute from './Routes/post.route.js';
-// import messageRoute from './Routes/message.route.js';
-// import cookieParser from 'cookie-parser';
-// import { app ,server } from './socket/socket.js';
-// import path from 'path'
-// dotenv.config();
-// const PORT = process.env.PORT;
+import express, { urlencoded } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './Utils/db.js';
+import userRoute from './Routes/user.route.js';
+import postRoute from './Routes/post.route.js';
+import messageRoute from './Routes/message.route.js';
+import cookieParser from 'cookie-parser';
+import { app ,server } from './socket/socket.js';
+import path from 'path'
 
-// const __dirname = path.resolve();
-
-
-
-// app.get("/", (req,res)=>{
-//   return res.status(200).json({
-//     message:"I'm comminging from BackEnd",
-//     success:true
-//   })
-// })
-
-// //middlewares
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(urlencoded({extended:true}));
-
-// const corsOptions ={
-//   origin:'http://localhost:5173',
-//   credentials:true,
-// }
-// app.use(cors(corsOptions));
-
-
-// app.use("/api/v1/user",userRoute)
-// app.use("/api/v1/post",postRoute)
-// app.use("/api/v1/message",messageRoute)
-
-// app.use(express.static(path.join(__dirname,"/FrontEnd/dist")))
-
-// app.get("*",(res,req)=>{
-//   res.sendFile(path.resolve(__dirname,"FrontEnd","dist","index.html"))
-// })
-// server.listen(PORT, ()=>{
-//   connectDB();
-//   console.log(`Server listen on ${PORT}`);
-  
-// })
-
-import express, { urlencoded } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import postRoute from "./routes/post.route.js";
-import messageRoute from "./routes/message.route.js";
-import { app, server } from "./socket/socket.js";
-import path from "path";
- 
 dotenv.config();
 
 
